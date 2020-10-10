c715111e6cf310dd2685a6e94febe403bf4b788c
Grzegorz Andrejczyn <grzegorz.andrejczyn@allegro.pl>
2020-10-10 11:16:30 +0200

    add bootstrap layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,44 @@
-import { Component } from 'react';
+import {Component} from 'react';
 import * as React from "react";
-import { Form} from "./Form";
+import {Form} from "./Form";
 
 export default class App extends Component {
     render() {
-        return (<Form />)
+        return (
+            <>
+                <div
+                    className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+                    <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3">KCal</a>
+                </div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                            <div className="sidebar-sticky pt-3">
+                                <ul className="nav flex-column">
+                                    <li className="nav-item">
+                                        <li className="nav-item">
+                                            <a className="nav-link active" href="#">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                     viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                     strokeWidth="2"
+                                                     strokeLinecap="round" strokeLinejoin="round"
+                                                     className="feather feather-shopping-cart">
+                                                    <circle cx="9" cy="21" r="1"></circle>
+                                                    <circle cx="20" cy="21" r="1"></circle>
+                                                    <path
+                                                        d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6">
+
+                                                    </path>
+                                                </svg>
+                                                Products
+                                            </a>
+                                        </li>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>)
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import * as _ from 'lodash';
 import App from "./components/App";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
-
+import 'bootstrap/dist/css/bootstrap.css'
+import './style.css'
 ReactDOM.render(<App />, document.body)
