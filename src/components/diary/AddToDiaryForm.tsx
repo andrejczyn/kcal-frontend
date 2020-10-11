876e04fcd0ebb3c30a3c6aab232d0441226f87cc
Grzegorz Andrejczyn <grzegorz.andrejczyn@allegro.pl>
2020-10-11 19:26:52 +0200

    navigate thrue autosuggest

--- src/components/diary/AddToDiaryForm.tsx
+++ src/components/diary/AddToDiaryForm.tsx
@@ -1,7 +1,9 @@
+import * as classNames from "classnames";
 import * as React from "react";
-import {ChangeEvent, ChangeEventHandler, useState} from "react";
+import {ChangeEvent, KeyboardEvent, useState} from "react";
 import {Product, ProductsService} from "../../services/Services";
 import "./add-to-diary-form.css"
+import * as _ from 'lodash';
 
 interface AddToDiaryFormProps {
     productService: ProductsService
@@ -10,15 +12,19 @@ interface AddToDiaryFormProps {
 export function AddToDiaryForm(props: AddToDiaryFormProps) {
     const [searchResult, setSearchResult] = useState<Product[]>([])
     const [query, setQuery] = useState('')
-
+    const [selected, setSelected] = useState<number>(null)
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
         const q = e.target.value;
         setQuery(q)
         if (!q) {
             setSearchResult([]);
+            setSelected(null);
         } else {
             props.productService.products({q: q})
-                .then((products) => setSearchResult(products.products));
+                .then((products) => {
+                    setSearchResult(products.products);
+                    setSelected(null);
+                });
         }
     }
 
@@ -32,13 +38,47 @@ export function AddToDiaryForm(props: AddToDiaryFormProps) {
         return <>{start}<b>{mid}</b>{end}</>;
     }
 
-    return (<form className="">
+    const onKeyPress = (e: KeyboardEvent<HTMLFormElement>) => {
+        if(searchResult.length == 0) {
+            setSelected(null);
+            return;
+        }
+        if(e.key == 'ArrowDown') {
+            if(selected == null) {
+                setSelected(0)
+                return;
+            }
+
+            if(selected == searchResult.length -1) {
+                setSelected(0)
+            } else {
+                setSelected(selected+1)
+            }
+            return;
+        }
+
+        if(e.key == 'ArrowUp') {
+            if(selected == null) {
+                setSelected(searchResult.length - 1 )
+                return;
+            }
+            if(selected == 0) {
+                setSelected(searchResult.length -1)
+            } else {
+                setSelected(selected-1)
+            }
+            return;
+        }
+
+    }
+
+    return (<form className="" onKeyDown={onKeyPress}>
         <div className="form-group mb-3">
             <input className="add-to-diary-control form-control" onChange={onChange} type="text"
                    placeholder="Add product to your diary"/>
             <ul className="list-group products-auto-suggest">
                 {searchResult.map((product) => <li key={product.id}
-                                                   className="list-group-item">{productName(query, product.name)}</li>)}
+                                                   className={classNames(["list-group-item",searchResult[selected]?.id == product.id && "active"])}>{productName(query, product.name)}</li>)}
             </ul>
         </div>
 
