d77e75755ec1042a7f85adb025b09bfd059f348f
Grzegorz Andrejczyn <grzegorz.andrejczyn@allegro.pl>
2020-10-11 18:57:55 +0200

    show query text

--- src/components/diary/AddToDiaryForm.tsx
+++ src/components/diary/AddToDiaryForm.tsx
@@ -2,28 +2,44 @@ import * as React from "react";
 import {ChangeEvent, ChangeEventHandler, useState} from "react";
 import {Product, ProductsService} from "../../services/Services";
 import "./add-to-diary-form.css"
+
 interface AddToDiaryFormProps {
     productService: ProductsService
 }
 
 export function AddToDiaryForm(props: AddToDiaryFormProps) {
     const [searchResult, setSearchResult] = useState<Product[]>([])
+    const [query, setQuery] = useState('')
 
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        if(!e.target.value) {
+        const q = e.target.value;
+        setQuery(q)
+        if (!q) {
             setSearchResult([]);
+        } else {
+            props.productService.products({q: q})
+                .then((products) => setSearchResult(products.products));
         }
-        props.productService.products({q: e.target.value})
-            .then((products) => setSearchResult(products.products));
+    }
+
+    const productName = (query: string, name: string) => {
+        const index = name.toLowerCase().indexOf(query.toLowerCase());
+        const start = name.substr(0, index);
+        const mid = name.substr(index, query.length);
+        const end = name.substr(index + query.length);
+
+
+        return <>{start}<b>{mid}</b>{end}</>;
     }
 
     return (<form className="">
         <div className="form-group mb-3">
             <input className="add-to-diary-control form-control" onChange={onChange} type="text"
                    placeholder="Add product to your diary"/>
-                   <ul className="list-group products-auto-suggest">
-                        {searchResult.map((product) => <li className="list-group-item">{product.name}</li>)}
-                   </ul>
+            <ul className="list-group products-auto-suggest">
+                {searchResult.map((product) => <li key={product.id}
+                                                   className="list-group-item">{productName(query, product.name)}</li>)}
+            </ul>
         </div>
 
     </form>)
